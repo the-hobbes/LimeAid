b6d973870e2496feb8b3723aca8c6ea37ac9efb3
the-hobbes <phelan.vendeville@gmail.com>
2012-06-04 21:05:32 -0400

    generalized to local version, not live server version

--- README.md
+++ README.md
@@ -2,3 +2,4 @@ LimeAid
 =======
 
 HTML5 App used to determine out of date Wordpress entries and send notifications.
+Limeaid is refreshing, and that's exactly what we want to do to the blogs. 
--- getXml.js
+++ getXml.js
@@ -1,15 +1,13 @@
 /**
  * XML_Download
  * A class used to create instances of objects which facilitate the automated downloading of XML representing the blog posts of a wordpress site.
- * Requires a username, password, and url to be supplied to the class to create an object.
+ * Requires a url to be supplied to the class to create an object.
  * Parameters: 
- * 		wordpress admin username, wordpress admin password, xml source url
+ * 		wordpress xml source url
  */
-function XML_Download(username_in, password_in, url_in)
+function XML_Download(url_in)
 {
 	//create and instantiate global variables via parameters
-	this.username = username_in;
-	this.password = password_in;
 	this.url = url_in;
 
 	/**
@@ -19,29 +17,21 @@ function XML_Download(username_in, password_in, url_in)
 	 */
 	 this.get_xml = function()
 	 {
-	 	//create a request object
-	 	var request = new XMLHttpRequest();
-	 	
-	 	//use open method to set up the request with a url and the type of request to use
-	 	request.open("GET", this.url);
-	 	
-	 	/**
-	 	 * Function onload
-	 	 * Using built in onload function, creates a handler to await the retrieval of the data by the request object.
-	 	 * When the browser gets an answer from the remote service, it calls this function.
-	 	 * If the data is successfully retured, the status code is 200. 
-	 	 */
-	 	 request.onload = function()
-	 	 {
-	 	 	if(request.status == 200)
-	 	 	{
-	 	 		//alert("Data Recieved");
-	 	 		alert(request.responseText);
-	 	 	}
-	 	 }
+		if (window.XMLHttpRequest)
+		{
+			// code for IE7+, Firefox, Chrome, Opera, Safari
+			xmlhttp=new XMLHttpRequest();
+		}
+		else
+		{
+			// code for IE6, IE5
+			xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+		}
 
-	 	 //send the request to the server (passing null to the remote service as we are not sending any data to it).
-	 	 request.send(null);
+		xmlhttp.open("GET", this.url, false);
+		xmlhttp.send();
+		xmlDoc = xmlhttp.responseXML; 
+		//alert(xmlDoc);
 	 }
 
 	 /**
@@ -58,12 +48,10 @@ function XML_Download(username_in, password_in, url_in)
 var debug = false;
 
 //change these as needed
-var user = "pvendevi";
-var pass = "";
-var url = "http://www.uvm.edu/~pvendevi/wp-admin/export.php?download";
+var url = "wordpress.2012-06-04.xml";
 
 //create connector object
-var objConnector = new XML_Download(user, pass, url);
+var objConnector = new XML_Download(url);
 
 //call the get_xml function
 objConnector.get_xml();
