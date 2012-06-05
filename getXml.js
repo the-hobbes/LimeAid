78e0f9a7a7e63f9e88378505b05d8c0def054f10
the-hobbes <phelan.vendeville@gmail.com>
2012-06-05 09:28:47 -0400

    finished xml parsing function

--- getXml.js
+++ getXml.js
@@ -20,12 +20,12 @@ function XML_Download(url_in)
 		if (window.XMLHttpRequest)
 		{
 			// code for IE7+, Firefox, Chrome, Opera, Safari
-			xmlhttp=new XMLHttpRequest();
+			xmlhttp = new XMLHttpRequest();
 		}
 		else
 		{
 			// code for IE6, IE5
-			xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+			xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
 		}
 
 		xmlhttp.open("GET", this.url, false);
@@ -41,31 +41,95 @@ function XML_Download(url_in)
 	  */
 	  this.test = function()
 	  {
-	  	alert(this.username + " " + this.password + " " + this.url);
+	  	alert(this.url);
 	  }
 }
+
+/**
+ * ParseXML
+ * A function used to parse the desired XML sections from the xml object (date and title nodes).
+ * Parameters:
+ * 		xml object returned by the XMLHttpRequest
+ */
+ function ParseXML(xmlObject_in)
+ {
+ 	//declare variables
+ 	var xmlObject = xmlObject_in;
+ 	var titleElements;
+ 	var pubDateElements;
+
+ 	this.titleElements = xmlObject.getElementsByTagName("title");
+ 	this.pubDateElements = xmlObject.getElementsByTagName("pubDate");
+
+ 	//declare arrays to store titles and pubdates
+ 	var titleArray = new Array();
+ 	var datesArray = new Array();
+
+ 	//add elements to the arrays
+ 	for(i = 0; i < this.titleElements.length; i++)
+ 	{
+ 		titleArray.push(this.titleElements[i].childNodes[0].nodeValue);
+ 		datesArray.push(this.pubDateElements[i].childNodes[0].nodeValue);
+ 	}
+
+ 	/**
+ 	 * getTitles
+ 	 * Returns: titleArray, the array of parsed xml titles
+ 	 */
+ 	this.getTitles = function()
+ 	{
+ 		return titleArray;
+ 	}
+
+	/**
+ 	 * getDates
+ 	 * Returns: datesArray, the array of parsed xml dates
+ 	 */
+ 	this.getDates = function()
+ 	{
+ 		return datesArray;
+ 	}
+
+ 	/**
+ 	 * Code to test the contents of the title and publication date variables, and the arrays.
+ 	 * Used for debugging.
+ 	 */
+ 	if(debug)
+ 	{
+		for (i = 0; i < this.titleElements.length; i++)
+		{ 
+			document.write(this.titleElements[i].childNodes[0].nodeValue, " :: ");
+			document.write(this.pubDateElements[i].childNodes[0].nodeValue);
+			document.write("<br />");
+
+			document.write(titleArray[i], " :: ");
+			document.write(datesArray[i]);
+ 			document.write("<br />");	
+		}
+	}
+ }
 
 //for debugging
 var debug = false;
 
-//change these as needed
+//change this as needed. The location of the xml file.
 var url = "wordpress.2012-06-04.xml";
 
 //create connector object
-var objConnector = new XML_Download(url);
-
-//call the get_xml function
-xmlObject = objConnector.get_xml();
-
-x = xmlObject.getElementsByTagName("title");
-for (i=0;i<x.length;i++)
-{ 
-	document.write(x[i].childNodes[0].nodeValue);
-	document.write("<br />");
-}
+var obj_Connector = new XML_Download(url);
 
 //for debugging
 if (debug == true)
 {
 	objConnector.test();
 }
+
+//call the get_xml function
+xml_Object = obj_Connector.get_xml();
+
+//call the parsing function, and setup the title and date arrays.
+var parse_Object = new ParseXML(xml_Object);
+var title_array = parse_Object.getTitles;
+var date_array = parse_Object.getDates;
+
+
