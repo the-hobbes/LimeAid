6a8f6594700cab58a1f2689696b239de628bac88
the-hobbes <phelan.vendeville@gmail.com>
2012-06-12 22:01:32 -0400

    added include file for database connection

--- scripts/databaseConnect.php
+++ scripts/databaseConnect.php
@@ -1,11 +1,3 @@
 <?php
-/*include("/usr/local/uvm-inc/limeaid_connect.inc");*/
-
-$connectId = mysql_connect("webdb.uvm.edu","helpline_admin","7iC6BbZhUEwUs9st");
-if (!$connectId)
-  {
-  	die('Could not connect: ' . mysql_error());
-  }
-mysql_select_db("HELPLINE_Limeaid", $connectId);
-
+include("/usr/local/uvm-inc/helpline.inc");
 ?>
