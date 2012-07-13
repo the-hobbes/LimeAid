756d5855675cc1cebea22dbb541eadccef1d3d4c
the-hobbes <phelan.vendeville@gmail.com>
2012-07-13 10:50:02 -0400

    email send button now synchronizes tracking with database

--- scripts/emailArticles.php
+++ scripts/emailArticles.php
@@ -1,10 +1,14 @@
 <?php
+	include("scripts/databaseConnect.php");
+
 	/**
 	 * Form handler called by "Email Selected Assignees" button.
 	 * Grabs checked assinees and checked articles, and sends an email to the assignees.
 	 */
 	if (isset($_POST["mailer_form"]))
 	{
+		$debug = False;
+
 		//$_POST["mailer_form"];
 		$checked_boxes = $_POST['assignee_email'];
 		$checked_articles = $_POST['article_checkbox'];
@@ -16,7 +20,6 @@
 			echo '<script type="text/javascript">alert("Message successfully sent.");</script>';
 		}
 
-		/*
 		//are we debugging?
 		if($debug)
 		{
@@ -49,7 +52,7 @@
 					echo($checked_articles[$i] . " ");
 				}
 			}
-		}//end debug*/
+		}//end debug
 	}
 
 /**
@@ -105,7 +108,7 @@ function makeEmail($checked_boxes_in, $checked_articles_in)
 		}
 	}
 	$message .= "<p></p>";
-	$message .="<p> Since this is an automated message, if you have any questions, please contact Carol Caldwell-Edmonds, or Travis Bartlett";
+	$message .="<p> Since this is an automated message, if you have any questions please contact Carol Caldwell-Edmonds or Travis Bartlett";
 	$message .-"</body></html>";
 	
 	//headers
@@ -113,11 +116,83 @@ function makeEmail($checked_boxes_in, $checked_articles_in)
     $headers .= "Content-type: text/html; charset=iso-8859-1\r\n";
 	$headers .= "From: Limeaid Blog Management\r\n";
 
-	// and now to mail it
+	//now to mail it
     $mailIt = mail($recipients, $subject, $message, $headers);
 
+    //update the tracking database
+    updateDatabase($checked_boxes_in, $checked_articles_in);
 	return true;
 }
 
+/**
+ * updateDatabase($checked_boxes_in, $checked_articles_in)
+ * Function used to add the information of the assignment of the article to the database, specifically in the table_progress.
+ */
+function updateDatabase($checked_boxes_in, $checked_articles_in)
+{
+	$name_array = array();
+	$article_array = array();
 
+	//FILL ARRAY: for each recipient, get the full name and put it into an array
+	for($i=0; $i < count($checked_boxes_in); $i++)
+	{
+		$email = $checked_boxes_in[$i];
+		
+		//look up the recipient's full name by thier email
+		$name_sql = "SELECT fld_firstname, fld_lastname FROM table_assignee WHERE pk_email = '$email'";
+		$name_result = mysql_query($name_sql) or die ("Unable to retrieve a record from table progess " . mysql_error());
+		$row = mysql_fetch_array($name_result);
+		$fullName = $row['fld_firstname'] . " " . $row['fld_lastname'];
+		$name_array[] = $fullName;
+	}
+
+	//FILL ARRAY: for each article, get the article name and put it into an array
+	for($i=0; $i < count($checked_articles_in); $i++)
+	{
+		$rawArticle = deleteFirstFiveChar($checked_articles_in[$i]);
+		$article = removeRemainingTag($rawArticle);
+		$article_array[] = $article;
+	}
+
+	//DO WORK (add to database): for each recipient...
+	for($i=0; $i < count($name_array); $i++)
+	{
+		//for each article, put name, article, and date into the database
+		for($z=0; $z < count($article_array); $z++)
+		{
+			$date = date('l jS \of F Y h:i:s A');
+			$name = $name_array[$i];
+			$article = $article_array[$z];
+
+			$insert_sql = "INSERT INTO table_progress (fld_name, fld_articleAssigned, fld_dateAssigned) VALUES ('$name', '$article', '$date')";
+
+			//run the query
+	    	mysql_query($insert_sql); 
+		}
+	}
+	
+}
+
+/**
+ * deleteFirstFiveChar($string)
+ * used to remove the <div> tag added to the blog title
+ */
+function deleteFirstFiveChar($string) 
+{
+	//used to remove <div> tag from the string
+	return substr( $string, 5);
+}
+
+/**
+ * removeRemainingTag($string)
+ * used to remove everything after the final '<' character in the title
+ */
+function removeRemainingTag($string)
+{
+	//position of < in div tag
+	$findme = "<";
+	$pos = strpos($string, $findme);
+
+	return substr($string, 0, $pos);
+}
 ?>
